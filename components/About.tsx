1819a48dbae75785766370a2021b2990efe85b12
Max Xue <maxxue1512@gmail.com>
2022-09-29 14:54:01 +1000

    did basic termial cursor typign animation

--- components/About.tsx
+++ components/About.tsx
@@ -5,13 +5,19 @@ import styles from '../styles/About.module.css'
 export const About = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.flex}><h1 className={styles.hello}>Hello, my name is <span className={styles.name}>Max Xue</span>.</h1></div>
+      {/* <div className={styles.flex}><h1 className={styles.hello}>Hello, my name is <span className={styles.name}>Max Xue</span>.</h1></div>
       <div className={styles.flex}>
         <h1 className={styles.secondaryH1}>I am a fullstack engineer. </h1>
-        {/* <span className={styles.cursor}></span> */}
-      </div>
+        <span className={styles.cursor}></span>
+      </div> */}
       
-
+      <h1 className={styles.hello}>
+        {/* <div className={styles.cursor}></div> */}
+        Hello, my name is <span className={styles.name}>Max Xue</span>.
+      
+      
+      </h1>
+      <h1 className={styles.secondaryH1}>I am a <span className={styles.fullstack}>fullstack</span> engineer. </h1>
       
 
 
