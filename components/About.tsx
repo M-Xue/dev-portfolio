17308b25b55c13e9c16a44a94b311e16649065de
Max Xue <maxxue1512@gmail.com>
2022-09-29 23:58:32 +1000

    made the landing screen text fully responsive

--- components/About.tsx
+++ components/About.tsx
@@ -5,22 +5,15 @@ import styles from '../styles/About.module.css'
 export const About = () => {
   return (
     <div className={styles.container}>
-      {/* <div className={styles.flex}><h1 className={styles.hello}>Hello, my name is <span className={styles.name}>Max Xue</span>.</h1></div>
-      <div className={styles.flex}>
-        <h1 className={styles.secondaryH1}>I am a fullstack engineer. </h1>
-        <span className={styles.cursor}></span>
-      </div> */}
       
-      <h1 className={styles.hello}>
-        <span className={styles.nowrap}>Hello, my name is</span> <span className={styles.name}>Max Xue</span>.
+      <h1 className={styles.h1}>
+        <span className={styles.hello}>Hello, my name is&nbsp;</span><span className={styles.name}><span className={styles.blueFont}>Max Xue</span></span>
       </h1>
-      <h1 className={styles.secondaryH1}>
-        <span className={styles.nowrap}>I am a <span className={styles.fullstack}>fullstack</span></span> engineer.
+      <h1 className={styles.h1}>
+        <span className={styles.fullstack}>I am a <span className={styles.yellowFont}>fullstack&nbsp;</span></span> <span className={styles.engineer}>engineer</span>
       </h1>
       
 
-
-      <p></p>
 
       
 
@@ -28,7 +21,3 @@ export const About = () => {
   )
 }
 
-// <span className={styles.test}></span>
-
-        /* <div className={styles.cursor}></div> */
-        /* <span className={styles.nowrap}></span> */
