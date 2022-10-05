da6b165a015f048b4d44bdce715cac1f50d06053
Max Xue <maxxue1512@gmail.com>
2022-10-06 01:16:56 +1100

    finished sidebar left animation

--- components/About.tsx
+++ components/About.tsx
@@ -5,6 +5,12 @@ import styles from '../styles/About.module.css'
 export const About = () => {
   return (
     <div>vbxcv
+
+      <br />
+
+      I'm a computer science 
+
+
       <br /><br /><br />
       <br /><br /><br />
       <br /><br /><br />
--- components/LeftSiderbar.tsx
+++ components/LeftSiderbar.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import styles from '../styles/LeftSidebar.module.css'
+import { motion, AnimatePresence } from "framer-motion"
 
 export const LeftSiderbar = () => {
   return (
-    <div className={styles.container}>
+    <motion.div 
+      className={styles.container}
+      initial={{ opacity: 0, y: '4px' }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: '4px' }}
+      transition={{ duration: 0.5, ease: [0.645, 0.045, 0.355, 1]}}
+    >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-terminal"><polyline points="4 17 10 11 4 5"></polyline><line x1="12" y1="19" x2="20" y2="19"></line></svg>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-code"><polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline></svg>
@@ -12,6 +19,6 @@ export const LeftSiderbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
 
         <div className={styles.vertLine}></div>
-    </div>
+    </motion.div>
   )
 }
