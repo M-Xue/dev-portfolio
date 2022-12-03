b39b40a0c739058ab3f26c355a9ea5f67b686610
Max Xue <maxxue1512@gmail.com>
2022-12-03 14:03:04 +1100

    using css transtion not working

--- components/sections/education/Education.tsx
+++ components/sections/education/Education.tsx
@@ -10,8 +10,8 @@ export const Education = () => {
   const [isContentOpen, setIsContentOpen] = useState(false);
 
   const handleButtonClick = () => setIsContentOpen(!isContentOpen);
-  const nodeRef = useRef(null);
-  const nodeRefText = useRef(null);
+  const eduModalRef = useRef(null);
+  const backdropRef = useRef(null);
   
 
   return (
@@ -27,138 +27,63 @@ export const Education = () => {
             I am also the Education Executive of CSESoc for 2023, where I lead multiple subcommittees to foster an inclusive and self-driven student developer community.
           </p> 
 
-          <button className={styles.educationPageButton} onClick={handleButtonClick}>
+          <button className={styles.educationPageButton} onClick={()=>setIsContentOpen(true)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-align-justify"><line x1="21" y1="10" x2="3" y2="10"/><line x1="21" y1="6" x2="3" y2="6"/><line x1="21" y1="14" x2="3" y2="14"/><line x1="21" y1="18" x2="3" y2="18"/></svg>
               <span>Read more here</span> 
           </button>
 
+          
 
-          {/* <div className={styles.box1}>
-
-            <div className={styles.box2}>
-              Beginning my Computer Science degree in September 2020, I developed a passion for software development learning the fundementals of programming. As my first term progressed, I developed a sense of pride looking back at the work I had completed and how I had created programs I could never have even imagined a few months prior. However, I knew this was just the beginning and developed a curiosity for what more I could create, diving into online free tutorials and ending up in the world of web applications, learning fullstack development with the MERN stack, as well as experimenting with mobile development with Swift.
-              This curiosity has stayed with me to this day as I strive to develop as a developer, learning new technologies to create more powerful and enjoyable experiences for users, both in streamlining perfomance for both backend and frontend as well as attempting to learn best practices to write clean and elegant code that is flexible, maintainable and extensible. 
-              As I progressed on my self-learning journey, I decided to join the UNSW Education Subcomittee in 2022 to share all the knowledge I had learnt in order to help other students overcome the struggles I faced trying to teach myself as I did not have many people to talk to about web application development since it is not a topic well covered by the UNSW Computer Science degree curriculum and there is an information overload online when you don't know what you are looking for. Here, I found many people as passionate as I am about software development and not only achieved my goals of helping other students but also experienced so much personal growth both as an individual and as a software developer.  
-              Inspired by my team and my directors, I applied and assumed the position of Education/Development Executve of CSESoc 2023, where I oversee 5 subcomittees dedicated to the growth and support of the developer community at UNSW. I hope to cultivate a developer community that thrives on the curiosity of passionate students and support students to achieve their goals as software developers by making informating accessible as well as creating opporunities for students to bond with like-minded students so we can all developer and grow from each other, leveraging our unique experience and knowledge.  
-            </div>
-
-          </div> */}
-
-
-
-
-
-
-
-
-
-          <div className={styles.testContainer}>
-            <CSSTransition
-              in={isContentOpen}
-              nodeRef={nodeRef}
-              // appear={true}
-              timeout={1000} 
-              // unmountOnExit
-              classNames={{
-                enter: styles.MyClassEnter,
-                enterActive: styles.MyClassEnterActive,
-                enterDone: styles.MyClassEnterDone,
-                exit: styles.MyClassExit,
-                exitActive: styles.MyClassExitActive
-              }}
-            >
-              <div className={styles.expand} ref={nodeRef}></div>
-
-            </CSSTransition>
-
-            <div className={styles.bigBoi}>
-
-              <CSSTransition
-                in={isContentOpen}
-                nodeRef={nodeRefText}
-                // appear={true}
-                timeout={2000} 
-                // unmountOnExit
-                classNames={{
-                  enter: styles.TextEnter,
-                  enterActive: styles.TextEnterActive,
-                  enterDone: styles.TextEnterDone,
-                  exit: styles.TextExit,
-                  exitActive: styles.TextExitActive
-                }}
-              >
-                <div className={styles.text} ref={nodeRefText}>
-                    Beginning my Computer Science degree in September 2020, I developed a passion for software development learning the fundementals of programming. As my first term progressed, I developed a sense of pride looking back at the work I had completed and how I had created programs I could never have even imagined a few months prior. However, I knew this was just the beginning and developed a curiosity for what more I could create, diving into online free tutorials and ending up in the world of web applications, learning fullstack development with the MERN stack, as well as experimenting with mobile development with Swift.
-                    This curiosity has stayed with me to this day as I strive to develop as a developer, learning new technologies to create more powerful and enjoyable experiences for users, both in streamlining perfomance for both backend and frontend as well as attempting to learn best practices to write clean and elegant code that is flexible, maintainable and extensible. 
-                    As I progressed on my self-learning journey, I decided to join the UNSW Education Subcomittee in 2022 to share all the knowledge I had learnt in order to help other students overcome the struggles I faced trying to teach myself as I did not have many people to talk to about web application development since it is not a topic well covered by the UNSW Computer Science degree curriculum and there is an information overload online when you don't know what you are looking for. Here, I found many people as passionate as I am about software development and not only achieved my goals of helping other students but also experienced so much personal growth both as an individual and as a software developer.  
-                    Inspired by my team and my directors, I applied and assumed the position of Education/Development Executve of CSESoc 2023, where I oversee 5 subcomittees dedicated to the growth and support of the developer community at UNSW. I hope to cultivate a developer community that thrives on the curiosity of passionate students and support students to achieve their goals as software developers by making informating accessible as well as creating opporunities for students to bond with like-minded students so we can all developer and grow from each other, leveraging our unique experience and knowledge.  
-                </div>
-
-              </CSSTransition>
-
-
-            </div>
-
-          </div>
-          <div>testfdfsfasd</div>
-
-
-
-              {/* * theres a bug with the hello my name is max animation cuz of the background position of the main page. fix after you clean up this */}
-
-
-                {/* New issue is that we cant get rid of the text div from the flow of the DOM
-                  solution: make the contianer and text div -999 z index but make the actual box be 0 z index or whatever the default z index is (maybe use auto)
-                */}
-
-
-
-
-{/* 
           <CSSTransition
             in={isContentOpen}
-            nodeRef={nodeRef}
-            appear={true}
+            nodeRef={backdropRef}
             timeout={1000} 
             unmountOnExit
             classNames={{
-              enter: styles.MyClassEnter,
-              enterActive: styles.MyClassEnterActive,
-              enterDone: styles.MyClassEnterDone,
-              exit: styles.MyClassExit,
-              exitActive: styles.MyClassExitActive
+              enter: styles.backdropEnter,
+              enterActive: styles.backdropEnterActive,
+              enterDone: styles.backdropEnterDone,
+              exit: styles.backdropExit,
+              exitActive: styles.backdropExitActive
             }}
           >
-            <div className={styles.box} ref={nodeRef}>
-              <div className={styles.text}>
-                  Beginning my Computer Science degree in September 2020, I developed a passion for software development learning the fundementals of programming. As my first term progressed, I developed a sense of pride looking back at the work I had completed and how I had created programs I could never have even imagined a few months prior. However, I knew this was just the beginning and developed a curiosity for what more I could create, diving into online free tutorials and ending up in the world of web applications, learning fullstack development with the MERN stack, as well as experimenting with mobile development with Swift.
-                  This curiosity has stayed with me to this day as I strive to develop as a developer, learning new technologies to create more powerful and enjoyable experiences for users, both in streamlining perfomance for both backend and frontend as well as attempting to learn best practices to write clean and elegant code that is flexible, maintainable and extensible. 
-                  As I progressed on my self-learning journey, I decided to join the UNSW Education Subcomittee in 2022 to share all the knowledge I had learnt in order to help other students overcome the struggles I faced trying to teach myself as I did not have many people to talk to about web application development since it is not a topic well covered by the UNSW Computer Science degree curriculum and there is an information overload online when you don't know what you are looking for. Here, I found many people as passionate as I am about software development and not only achieved my goals of helping other students but also experienced so much personal growth both as an individual and as a software developer.  
-                  Inspired by my team and my directors, I applied and assumed the position of Education/Development Executve of CSESoc 2023, where I oversee 5 subcomittees dedicated to the growth and support of the developer community at UNSW. I hope to cultivate a developer community that thrives on the curiosity of passionate students and support students to achieve their goals as software developers by making informating accessible as well as creating opporunities for students to bond with like-minded students so we can all developer and grow from each other, leveraging our unique experience and knowledge.  
-              </div>
+            <div className={styles.backdrop} onClick={()=>setIsContentOpen(false)} ref={backdropRef}>
+              <CSSTransition
+                in={isContentOpen}
+                nodeRef={eduModalRef}
+                timeout={1000} 
+                unmountOnExit
+                classNames={{
+                  enter: styles.eduModalEnter,
+                  enterActive: styles.eduModalEnterActive,
+                  enterDone: styles.eduModalEnterDone,
+                  exit: styles.eduModalExit,
+                  exitActive: styles.eduModalExitActive
+                }}
+              >
+                <div className={styles.eduModal} ref={eduModalRef}>
+                    <p>
+                      Beginning my Computer Science degree in September 2020, I developed a passion for software development learning the fundementals of programming. As my first term progressed, I developed a sense of pride looking back at the work I had completed and how I had created programs I could never have even imagined a few months prior. However, I knew this was just the beginning and developed a curiosity for what more I could create, diving into online free tutorials and ending up in the world of web applications, learning fullstack development with the MERN stack, as well as experimenting with mobile development with Swift.
+                      This curiosity has stayed with me to this day as I strive to develop as a developer, learning new technologies to create more powerful and enjoyable experiences for users, both in streamlining performance for both backend and frontend as well as attempting to learn best practices to write clean and elegant code that is flexible, maintainable and extensible. 
+                      As I progressed on my self-learning journey, I decided to join the UNSW Education Subcomittee in 2022 to share all the knowledge I had learnt in order to help other students overcome the struggles I faced trying to teach myself as I did not have many people to talk to about web application development since it is not a topic well covered by the UNSW Computer Science degree curriculum and there is an information overload online when you don't know what you are looking for. Here, I found many people as passionate as I am about software development and not only achieved my goals of helping other students but also experienced so much personal growth both as an individual and as a software developer.  
+                      Inspired by my team and my directors, I applied and assumed the position of Education/Development Executive of CSESoc 2023, where I oversee 5 subcomittees dedicated to the growth and support of the developer community at UNSW. I hope to cultivate a developer community that thrives on the curiosity of passionate students and support students to achieve their goals as software developers by making information accessible as well as creating opportunities for students to bond with like-minded students so we can all developer and grow from each other, leveraging our unique experience and knowledge. 
+                    </p>
+                </div>
+              </CSSTransition>
             </div>
-          </CSSTransition> */}
+          </CSSTransition>
 
-          {/* 
-            put these two in the same div and keep the text one alway on the screen
-            use z-index??
+
           
-          */}
 
-
-
+          {/* Something wrong with the z index of the container. if it isnt -999, it covers all other elements on the screen */}
           
         </div>
-
-        {/* <div className={styles.courses}>
-          <TabList/>
-        </div> */}
-
+          
+                     
 
       </div>
 
     </div>
   )
 }
-
-
