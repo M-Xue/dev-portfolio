35dc50b2d3dedd91a24a2a36f63aba7efd8ca527
Max Xue <maxxue1512@gmail.com>
2023-01-21 01:38:06 +1100

    added email tooltip

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -21,10 +21,16 @@ const Topbar = forwardRef<HTMLDivElement>(
                       Skills
                     </a>
                   </li> */}
-                  <li className={styles.projects}>
+                  {/* <li className={styles.projects}>
                     <a href="">
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-code"><polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline></svg>
                       Projects
+                    </a>
+                  </li> */}
+                  <li className={styles.work}>
+                    <a href="">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-git-merge"><circle cx="18" cy="18" r="3"/><circle cx="6" cy="6" r="3"/><path d="M6 21V9a9 9 0 0 0 9 9"/></svg>
+                      Work Experience 
                     </a>
                   </li>
                   <li className={styles.education}>
--- components/sections/education/Course.tsx
+++ components/sections/education/Course.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import useHover from '../../../hooks/useHover'
 import styles from '../../../styles/sections/education/Course.module.css'
+import { CSSTransition } from 'react-transition-group'
 
 interface assignment {
   name: string,
@@ -10,35 +12,47 @@ interface course {
   title: string,
   description: string,
   technologies: string[],
-  assignments?: assignment[]
-}
-
-const t = {
-  title: "Databases",
-  description: "Learning the fundamentals of SQL queries with PostgreSQL, using normalization to reduce redundancy in ER and relational models",
-  technologies: ["SQL","PostgreSQL","PLpgSQL","psycopg2"]
+  assignments: string,
 }
 
 export const Course = ({course}: {course: course}) => {
+  const [ref, isHovered] = useHover<SVGSVGElement | null>();
+  const tooltipRef = useRef(null);
+
   return (
     <div className={styles.container}>
         <div className={styles.content}>
           <div className={styles.heading}>
             <h3 className={styles.title}>{course.title}</h3>
-            <div className={styles.repoContainer}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg></div>
+            <div 
+              className={styles.repoContainer}
+            >
+              <a href={course.assignments} target={"_blank"} rel={"noreferrer"}>
+                <svg ref={ref} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>
+              </a>
+
+              <CSSTransition
+                in={isHovered}
+                nodeRef={tooltipRef}
+                timeout={1300} 
+                unmountOnExit
+                classNames={{
+                  enter: styles.tooltipEnter,
+                  enterActive: styles.tooltipEnterActive,
+                  enterDone: styles.tooltipEnterDone,
+                  exit: styles.tooltipExit,
+                  exitActive: styles.tooltipExitActive
+                }}
+              >
+                <div className={styles.tooltip} ref={tooltipRef}>Assignment Repositories</div>
+              </CSSTransition>
+            </div>
           </div>
           <p className={styles.description}>{course.description}</p>
         </div>
         <ul className={styles.technologiesList}>
           {course.technologies.map((t:string, idx:number)=><li key={idx}>{t}</li>)}
         </ul>
-        {
-          course.assignments 
-          && 
-          <ul className={styles.assignments}>
-            {course.assignments.map((a:assignment, idx:number)=><li key={idx}><a href={a.repo} target="_blank" rel="noreferrer">{a.name}</a></li>)}
-          </ul>
-        }
     </div>
   )
 }
