4b8e4f5e3e601f4cc11ebb9b44fce7367bd085d1
Max Xue <maxxue1512@gmail.com>
2023-01-25 22:29:15 +1100

    added animations to top bar

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -15,18 +15,7 @@ const TopBar = forwardRef<HTMLDivElement>(
                       About
                     </a>
                   </li>
-                  {/* <li className={styles.skills}>
-                    <a href="">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-terminal"><polyline points="4 17 10 11 4 5"></polyline><line x1="12" y1="19" x2="20" y2="19"></line></svg>
-                      Skills
-                    </a>
-                  </li> */}
-                  {/* <li className={styles.projects}>
-                    <a href="">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-code"><polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline></svg>
-                      Projects
-                    </a>
-                  </li> */}
+
                   <li className={styles.work}>
                     <a href="">
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-git-merge"><circle cx="18" cy="18" r="3"/><circle cx="6" cy="6" r="3"/><path d="M6 21V9a9 9 0 0 0 9 9"/></svg>
@@ -62,3 +51,18 @@ const TopBar = forwardRef<HTMLDivElement>(
 
 TopBar.displayName='TopBar';
 export {TopBar};
+
+
+
+                  {/* <li className={styles.skills}>
+                    <a href="">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-terminal"><polyline points="4 17 10 11 4 5"></polyline><line x1="12" y1="19" x2="20" y2="19"></line></svg>
+                      Skills
+                    </a>
+                  </li> */}
+                  {/* <li className={styles.projects}>
+                    <a href="">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-code"><polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline></svg>
+                      Projects
+                    </a>
+                  </li> */}
